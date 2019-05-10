b54034826f9258ce27e4ce3545a7ca05c8f10541
Bartosz Majsak <bartosz.majsak@gmail.com>
2019-05-10 15:42:04 +0200

    fix: refactors playbook creation for only relevant tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,112 +1,133 @@
-'use strict'
+'use strict';
 
 /**
- * Loosely based on https://github.com/couchbase/docs-site/blob/aa1f5e5a07027047eface38bc3ae34f0690a82d3/gulpfile.js
+ * Derived from https://github.com/couchbase/docs-site/blob/aa1f5e5a07027047eface38bc3ae34f0690a82d3/gulpfile.js
  */
 const gulp = require('gulp');
 const connect = require('gulp-connect');
 const fs = require('fs');
-const exitHook = require('exit-hook')
+const exitHook = require('exit-hook');
+const open = require('open');
 const yaml = require('js-yaml');
-const commandLineArgs = require('command-line-args')
+const commandLineArgs = require('command-line-args');
 const generator = require('@antora/site-generator-default');
 
 const optionDefinitions = [
-  { name: 'source', alias: 's', type: String, defaultValue: 'site.yml' },
-  { name: 'playbook', alias: 'p', type: String },
-  { name: 'dir', alias: 'd', type: String, multiple: true },
-  { name: 'output', alias: 'o', type: String, defaultValue: 'docs-dev' },
-  { name: 'keep', alias: 'k', type: Boolean, defaultValue: false }
-]
-
-let [playbook, filename] = loadDevPlaybook()
-
-let args = [
-  "--playbook",
-  filename
+    {name: 'source', alias: 's', type: String, defaultValue: 'site.yml'},
+    {name: 'playbook', alias: 'p', type: String},
+    {name: 'dir', alias: 'd', type: String, multiple: true},
+    {name: 'output', alias: 'o', type: String, defaultValue: 'docs-dev'},
+    {name: 'keep', alias: 'k', type: Boolean, defaultValue: false}
 ];
 
-gulp.task('build', function (cb) {
-  /**
-   * Use the '@antora/site-generator-default' node module to build.
-   * It's analogous to `$ antora --playbook site-dev.yml`.
-   * Having access to the generator in code may be useful for other
-   * reasons in the future (i.e to implement custom features).
-   * NOTE: As opposed to building with the CLI, this method doesn't use
-   * a separate process for each run. So if a build error occurs with the `gulp`
-   * command it can be useful to check if it also happens with the CLI command.
-   */
-  generator(args, process.env)
-    .then(() => {
-      cb();
-    })
-    .catch(err => {
-      console.log(err);
-      cb();
-    });
+gulp.task('build', preparePlaybook(function (playbook, filename) {
+    /**
+     * Use the '@antora/site-generator-default' node module to build.
+     * It's analogous to `$ antora --playbook site-dev.yml`.
+     * Having access to the generator in code may be useful for other
+     * reasons in the future (i.e to implement custom features).
+     * NOTE: As opposed to building with the CLI, this method doesn't use
+     * a separate process for each run. So if a build error occurs with the `gulp`
+     * command it can be useful to check if it also happens with the CLI command.
+     */
+    let args = [
+        "--playbook",
+        filename
+    ];
+
+    return function (cb) {
+        generator(args, process.env).then(() => {
+            cb();
+        }).catch(err => {
+            console.log(err);
+            cb();
+        })
+    }
+}));
+
+gulp.task('preview', ['build'], preparePlaybook(function () {
+    /**
+     * Remove the line gulp.src('README.adoc')
+     * This is placeholder code to follow the gulp-connect
+     * example. Could not make it work any other way.
+     */
+    return function () {
+        gulp.src('README.adoc').pipe(connect.reload());
+    }
+}));
+
+gulp.task('watch', preparePlaybook(function (playbook, filename) {
+    return function () {
+        const dirs = playbook.content.sources
+            .map(source => [
+                `${source.url}/**/**.yml`,
+                `${source.url}/**/**.adoc`
+            ]);
+        dirs.push([filename]);
+        gulp.watch(dirs, ['preview']);
+    }
+}));
+
+gulp.task('connect', preparePlaybook(function (playbook) {
+    const connectOptions = {
+        name: `${playbook.site.title} Antora Doc Server`,
+        port: 5353,
+        livereload: true,
+        root: playbook.output.dir,
+    };
+    return function () {
+        connect.server(connectOptions);
+        open(`http://localhost:${connectOptions.port}`)
+    }
+}));
+
+gulp.task('open', function () {
+    open('docs/index.html');
 });
 
-gulp.task('preview', ['build'], function () {
-  /**
-   * Remove the line gulp.src('README.adoc')
-   * This is placeholder code to follow the gulp-connect
-   * example. Could not make it work any other way.
-   */
-  gulp.src('README.adoc')
-    .pipe(connect.reload());
-})
+gulp.task('default', ['connect', 'watch', 'build']);
 
-gulp.task('watch', function () {
-  let dirs = playbook.content.sources
-    .map(source => [
-      `${source.url}/**/**.yml`,
-      `${source.url}/**/**.adoc`
-    ]);
-  dirs.push([filename]);
-  gulp.watch(dirs, ['preview']);
-});
+function preparePlaybook(func) {
+    const options = commandLineArgs(optionDefinitions, {partial: true});
+    let filename = options.playbook;
+    let playbook = yaml.safeLoad(fs.readFileSync(options.source, 'UTF-8'));
 
-gulp.task('connect', function() {
-  let root = playbook.output.dir
-  let title = playbook.site.title
-  connect.server({
-    port: 5353,
-    name: `${title} Antora Doc Server`,
-    livereload: true,
-    root: root,
-  });
-});
+    if (!!!filename) {
+        filename = loadDevPlaybook(options, playbook)
+    }
 
-gulp.task('default', ['connect', 'watch', 'build'])
+    // returns a function which will be called by gulp task
+    return func(playbook, filename)
+}
 
 // Uses playbook specified through p/playbook option or creates one on the fly
-function loadDevPlaybook() {
-  const options = commandLineArgs(optionDefinitions, { partial: true })
-  var filename = options.playbook
-  let playbook = yaml.safeLoad(fs.readFileSync(options.source, 'UTF-8'));
+function loadDevPlaybook(options, playbook) {
 
-  if (!!!filename) {      
-  
     if (!!!options.dir) {
-      console.log('local clone of the repository is not specified, please clone it and point to it using -d or --dir flag')
-      process.exit(-1);
+        console.log('local clone of the repository is not specified, please clone it and point to it using -d or --dir flag');
+        process.exit(-1);
     }
-     
-    filename = `${__dirname}/site-local-dev.yml`
+
+    const filename = `${__dirname}/site-local-dev.yml`;
+    if (fs.existsSync(filename)) {
+        // we can use it
+        // this check also avoids doing the same file manipulations over and over again for sequence of tasks
+        return filename
+    }
+
     playbook.content.sources.forEach((e, i) => {
-      e.url = options.dir[i % options.dir.length];
+        e.url = options.dir[i % options.dir.length];
     });
-    playbook.output.dir = options.output
-    let yamlDoc = yaml.safeDump(playbook)
-    fs.writeFileSync(filename, yamlDoc) 
 
-    if (!options.keep) {    
-      exitHook(() => {
-        fs.unlinkSync(filename)
-      })
+    playbook.output.dir = options.output;
+    const yamlDoc = yaml.safeDump(playbook);
+    fs.writeFileSync(filename, yamlDoc);
+
+    if (!options.keep) {
+        exitHook(() => {
+            fs.unlink(filename);
+        });
     }
-  
-  }
 
-  return [playbook, filename]
+    return filename
 }
